274613ae1d1549752f3ec1c85029820d38bb99a9
JanKotynia <jasiek.kotynia@gmail.com>
2025-06-15 23:58:53 +0200

    register

--- shop/__init__.py
+++ shop/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "home"
+login_manager.login_message_category = "info"
 from shop import routes
--- shop/routes.py
+++ shop/routes.py
@@ -1,6 +1,28 @@
+from flask_login import login_user
 from shop import app
-from flask import render_template
+from flask import render_template, flash
+from shop import db
+from shop.forms import RegisterForm
+from shop.models import User
 
-@app.route("/")
+
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('welcome.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        login_user(user_to_create)
+        print("created")
+        flash(f"Created! {user_to_create}", category='success')
+
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print("err")
+            flash(f'Create user ERROR: {err_msg}', category='danger')
+
+    return render_template('welcome.html', form=form)
