83e83aeddee63612cc9ee24e5043c12a8e842a54
JanKotynia <jasiek.kotynia@gmail.com>
2025-06-15 23:58:42 +0200

    register

---
+++ shop/models.py
@@ -0,0 +1,24 @@
+from shop import db, login_manager
+from shop import bcrypt
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=50), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=50), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("No readable")
+
+    @password.setter
+    def password(self, password_plain):
+        self.password_hash = bcrypt.generate_password_hash(password_plain).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
