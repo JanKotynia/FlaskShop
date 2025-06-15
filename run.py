fc772dc8d86b6b9024feb0673762f676487d501a
JanKotynia <jasiek.kotynia@gmail.com>
2025-06-15 16:57:30 +0200

    welcome modal

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from shop import app
-from shop.chat import chat_bp
-app.register_blueprint(chat_bp)
 
 if __name__ == '__main__':
     app.run(debug=1)
--- shop/routes.py
+++ shop/routes.py
@@ -4,11 +4,3 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template('welcome.html')
-
-@app.route("/login")
-def login():
-    return render_template('login.html')
-
-@app.route("/register")
-def register():
-    return render_template('register.html')
