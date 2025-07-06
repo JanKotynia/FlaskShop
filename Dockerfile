664db637f17913c8a10fda7d70f95dd80c2d59da
JanKotynia <jasiek.kotynia@gmail.com>
2025-07-06 22:46:25 +0200

    dockerfile fix2

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,16 @@ FROM python:3.11-slim
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
-
-RUN python db_sql_querry.py
 
 ENV FLASK_APP=run.py
 ENV FLASK_RUN_HOST=0.0.0.0
 
+RUN python db_sql_querry.py
+
 EXPOSE 5000
 
+# Uruchom aplikację
 CMD ["flask", "run"]
--- db_sql_querry.py
+++ db_sql_querry.py
@@ -10,12 +10,13 @@ from shop.models import User, Item
 
 with app.app_context():
     db.create_all()
-    item = Item(name = "Coconut shell", price = 5, barcode = "barcode", description = "desc", amount = 40)
-    item2 = Item(name = "Coconut shell2", price = 15, barcode = "barcode2", description = "desc2", amount = 40)
-    item3 = Item(name = "Coconut shell3", price = 25, barcode = "barcode3", description = "desc3", amount = 40)
-    item4 = Item(name = "Coconut shell4", price = 35, barcode = "barcode4", description = "desc4", amount = 40)
-    db.session.add(item)
-    db.session.add(item2)
-    db.session.add(item3)
-    db.session.add(item4)
+    if not Item.query.first():
+        items = [
+            Item(name="Coconut shell", price=5, barcode="barcode", description="desc", amount=40),
+            Item(name="Coconut shell2", price=15, barcode="barcode2", description="desc2", amount=40),
+            Item(name="Coconut shell3", price=25, barcode="barcode3", description="desc3", amount=40),
+            Item(name="Coconut shell4", price=35, barcode="barcode4", description="desc4", amount=40),
+        ]
+        db.session.add_all(items)
+        db.session.commit()
     db.session.commit()
